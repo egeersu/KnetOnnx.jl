edb16c39add768f875f3a4409ad164fbb29ae727
Ege <egeersu@gmail.com>
2020-02-06 16:03:24 +0000

    mlp

--- test/testmlp.jl
+++ test/testmlp.jl
@@ -1,4 +1,7 @@
 using KnetOnnx
 
-file_path = "/Users/egeersu/Desktop/KnetOnnx/src/PosterDemo/cnn.onnx";
+file_path = "/Users/egeersu/Desktop/KnetOnnx/src/recurrent.onnx";
+graph = ONNXtoGraph(file_path)
+PrintGraph(graph)
 model = KnetModel(file_path)
+println(model.tensors)
