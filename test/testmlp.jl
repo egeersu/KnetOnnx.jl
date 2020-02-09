d0d299dd81d9ca714c3de05aec33ed101219aaac
Ege <egeersu@gmail.com>
2020-02-09 16:30:54 +0000

    fix folders

--- test/testmlp.jl
+++ test/testmlp.jl
@@ -1,6 +1,7 @@
 using KnetOnnx
 
-file_path = "/Users/egeersu/Desktop/KnetOnnx/src/recurrent.onnx";
+file_path = "/Users/egeersu/Desktop/KnetOnnx/src/PosterDemo/mlp.onnx"
+#file_path = "/Users/egeersu/Desktop/KnetOnnx/src/recurrent.onnx";
 graph = ONNXtoGraph(file_path)
 PrintGraph(graph)
 model = KnetModel(file_path)
